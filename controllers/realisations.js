5c7489ff2086f0e8cb17619d4c0463c3d2f42a93
Laetitia Aduriz <aduriz.laetitia@sfr.fr>
2022-06-01 17:06:22 +0200

    test use data from db + add links
    start to see how to use and display the data from MongoDB + add links on some pages

--- controllers/realisations.js
+++ controllers/realisations.js
@@ -1,3 +1,15 @@
-export default function realisations(req, res) {
+import mongoose from "mongoose";
+import {FurnitureModel} from "../Models/Furniture.js"
+
+export default async function realisations(req, res) {
+    const furniture = await FurnitureModel.find({})
+
+    // let test = []
+    // furniture.forEach(val =>{
+    //     test.push(val.name)
+    // })
+
+    // res.json(test)
+
     res.render("realisations")
 }
