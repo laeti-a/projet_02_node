9c31e5abd26a604cdafc8dc123cb74f84aadda00
Laetitia Aduriz <aduriz.laetitia@sfr.fr>
2022-06-02 15:16:48 +0200

    test upload img in app
    test to upload an image and add it to the img file in the app + new node modules added

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -1,7 +1,14 @@
 import { FurnitureModel } from '../Models/Furniture.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export async function CreateFurnitureController(req, res){
     const { name, description, type, img, wood, metal, plastic} = req.body
+
+    res.sendFile(path.join(__dirname, '../public/img/' + img))
 
     try {
         const newFurniture = await FurnitureModel.create({name, description, type, img, wood, metal, plastic})
@@ -9,7 +16,7 @@ export async function CreateFurnitureController(req, res){
     
         res.redirect('/realisations')
     } catch ({ message: errorMessage }) {
-        return res.status(400).render('home', { errorMessage, values: req.body });
+        return res.status(400).render('home')
     }
 
 }
