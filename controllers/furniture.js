4cea4d73b0a8d537b2f996dd16fd5503277452b6
Laetitia Aduriz <aduriz.laetitia@sfr.fr>
2022-06-02 17:27:36 +0200

    remove useless code

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -1,36 +1,35 @@
 import { FurnitureModel } from '../Models/Furniture.js'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import fs from 'fs'
 
-import multer from 'multer'
+// import multer from 'multer'
 
-// Code to upload images in the app //
-let storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-      cb(null, './public/img/')
-  },
-  filename: (req, file, cb) =>{
-      cb(null, file.originalname)
-  }
-})
-
-const fileFilter = (req, file, cb) =>{
-  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' ||file.mimetype === 'image/png'){
-      cb(null, true)
-  }
-  else{
-      cb(null, false)
-  }
-}
-let upload = multer({
-  storage:storage,
-  fileFilter:fileFilter
-})
+// // Code to upload images in the app //
+// let storage = multer.diskStorage({
+//   destination: (req, file, cb) =>{
+//       cb(null, '../public/img/')
+//   },
+//   filename: (req, file, cb) =>{
+//       console.log(file)
+//       cb(null, file.originalname)
+//   }
+// })
+// const fileFilter = (req, file, cb) =>{
+//   if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' ||file.mimetype === 'image/png'){
+//       cb(null, true)
+//   }
+//   else{
+//       cb(null, false)
+//   }
+// }
+// let upload = multer({
+//   storage:storage,
+//   fileFilter:fileFilter
+// })
 
 export async function CreateFurnitureController(req, res){
     const { name, description, type, img, wood, metal, plastic} = req.body
 
-    upload.single(img)
+    // upload.single(img)
 
     try {
         const newFurniture = await FurnitureModel.create({name, description, type, img, wood, metal, plastic})
